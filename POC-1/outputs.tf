33acd086fb91b32ceedb0f52a8a06fff2957f653
ayyappa1 <b.ayyappa@gmail.com>
2018-06-25 16:45:26 +0530

    Add files via upload

---
+++ POC-1/outputs.tf
@@ -0,0 +1,8 @@
+
+output "PublicIp" {
+  value = "${aws_instance.one.public_ip}"
+}
+
+output "privateip" {
+  value = "${aws_instance.one.private_ip}"
+}
---
+++ POC-1/var.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "accesskey" {
+  default = "*************"
+}
+
+variable "secretkey" {
+  default = "*************"
+}
+
+variable "imageid" {
+  default = "ami-41e9c52e"
+}
+
+variable "key" {
+  default = "stp-poc"
+}
+
+variable "instancetype" {
+  default = "t2.micro"
+}
